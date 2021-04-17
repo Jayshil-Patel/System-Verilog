f6a23261f653d407c5b448a76e088d45a7796c13
Jayshil Patel <78102199+Jayshil-Patel@users.noreply.github.com>
2021-04-17 15:59:40 +0530

    Add files via upload

---
+++ Adder/design.sv
@@ -0,0 +1,19 @@
+module adder() (
+    input clk     ,
+    input reset   ,
+    input [3:0] a ,
+    input [3:0] b ,
+    input valid   ,
+    output [6:0] c,
+);
+reg [6:0] tmp_c;
+
+//Reset
+always@(posedge reset)
+tmp_c <= 0;
+
+// Waddition operation
+always@(posedge clk)
+    if(valid)
+    
+endmodule
